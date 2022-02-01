cdab96b83751b9ed8bbe08350bad6286de666bd9
Anders <andrslauridsen@gmail.com>
2022-02-01 16:50:20 +0100

    introduction exercises

---
+++ 0.Introduction_exercises/2.Strings.js
@@ -0,0 +1,56 @@
+// --------------------------------------
+// Exercise 1 - Add numbers from string to float
+
+const numberOne = "1.10";
+const numberTwo = "2.30";
+
+// add those two numbers and show the result
+// you cannot touch line 1 neither line 2
+
+console.log(parseFloat(numberOne) + parseFloat(numberTwo));
+// --------------------------------------
+
+
+// --------------------------------------
+// Exercise 2 - Add the numbers and the total with 2 decimals
+
+const anotherNumberOne = "1.10";
+const anotherNumberTwo = "2.30";
+
+console.log((parseFloat(numberOne) + parseFloat(numberTwo)).toFixed(2));
+// --------------------------------------
+// Exercise 3 - Decimals and average
+
+const one = 10;
+const two = 45;
+const three = 98;
+
+// Show in the console the avg. with 5 decimals
+
+let total = one + two + three;
+let average = total/3;
+console.log(average.toFixed(5));
+
+// --------------------------------------
+// Exercise 4 - Get the character by index
+
+const letters = "abc";
+// Get me the character "c"
+
+Letter_C = letters.charAt(2);
+
+console.log(Letter_C);
+// --------------------------------------
+// Exercise 5 - Replace
+
+const fact = "You are learning javascript!";
+
+// capitalize the J in Javascript
+
+const replacement = fact.replace("javascript","Javascript");
+
+console.log(replacement);
+// --------------------------------------
+
+
+
